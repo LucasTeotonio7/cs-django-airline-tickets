84ec9c227f49ff94cf2408bb8a70715773554220
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-22 08:37:00 -0300

    added tempus-dominus

--- airlinetickets/settings.py
+++ airlinetickets/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tickets',
+    'tempus_dominus',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPUS_DOMINUS_LOCALIZE = True
+
--- tickets/forms.py
+++ tickets/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from tempus_dominus.widgets import DatePicker
+
 
 class TicketForms(forms.Form):
-    origin = forms.CharField(label='Origem', max_length=100)
+    origin = forms.CharField(label='Origem', max_length=100,)
     destiny = forms.CharField(label='Destino', max_length=100)
-    departure_date = forms.DateField(label='Ida')
-    date_back = forms.DateField(label='Volta')
-    
+    departure_date = forms.DateField(label='Ida', widget=DatePicker())
+    date_back = forms.DateField(label='Volta', widget=DatePicker())
+
