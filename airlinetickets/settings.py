cad45f9cfcd554047989537db65822c8cdb7de62
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-22 09:23:28 -0300

    chore: installed django-widget-tweaks

--- airlinetickets/settings.py
+++ airlinetickets/settings.py
@@ -34,7 +34,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tickets',
+
     'tempus_dominus',
+    'widget_tweaks',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
