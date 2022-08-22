0cb8a1b040b63cb9f8b5f51d9449c00d182f8da1
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-22 09:02:09 -0300

    feat: created new fields for the form

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from tickets.class_types import class_types
 
 
 class TicketForms(forms.Form):
@@ -7,4 +9,12 @@ class TicketForms(forms.Form):
     destiny = forms.CharField(label='Destino', max_length=100)
     departure_date = forms.DateField(label='Ida', widget=DatePicker())
     date_back = forms.DateField(label='Volta', widget=DatePicker())
+    date_search = forms.DateField(label='Data da Pesquisa', widget=DatePicker(), disabled=True, initial=datetime.today())
+    travel_class = forms.ChoiceField(label='Classe', choices=class_types)
+    informations = forms.CharField(
+        label='Informações extras',
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(label='email', max_length=150)
 
