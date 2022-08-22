509c34263d2865b7264c90fefd6d4d2cd37b79b4
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-22 08:07:34 -0300

    feat: created search results page

--- tickets/views.py
+++ tickets/views.py
@@ -7,3 +7,10 @@ def index(request):
     form = TicketForms()
     context = {'form':form}
     return render(request, 'index.html', context)
+
+
+def search_results(request):
+    if request.method == 'POST':
+        form = TicketForms(request.POST)
+        context = {'form':form}
+        return render(request, 'search_results.html', context)
