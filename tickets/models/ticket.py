341ac788bbc56309c6b9fb3f40a3216a78e3d96b
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-27 15:17:10 -0300

    feat: add verbose name in ticket model

--- tickets/models/ticket.py
+++ tickets/models/ticket.py
@@ -2,10 +2,10 @@ from django.db import models
 from .travel_class import ClassTypes
 
 class Ticket(models.Model):
-    origin = models.CharField(max_length=100)
-    destiny = models.CharField(max_length=100)
-    departure_date = models.DateField()
-    date_back = models.DateField()
-    date_search = models.DateField()
-    informations = models.TextField(max_length=200, blank=True)
-    travel_class = models.CharField(max_length=4,choices=ClassTypes.choices, default=0)
+    origin = models.CharField(verbose_name='Origem', max_length=100)
+    destiny = models.CharField(verbose_name='Destino', max_length=100)
+    departure_date = models.DateField(verbose_name='Data de ida')
+    date_back = models.DateField(verbose_name='Data de volta')
+    date_search = models.DateField(verbose_name='Data da pesquisa')
+    informations = models.TextField(verbose_name='Informações', max_length=200, blank=True)
+    travel_class = models.CharField(verbose_name='Classe', max_length=4,choices=ClassTypes.choices, default=0)
