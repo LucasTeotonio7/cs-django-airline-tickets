0d81a3711dc771327f6a954be334ec05d25f1bfb
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-27 11:46:50 -0300

    feat: alter field travel_class on ticket

--- tickets/models/travel_class.py
+++ tickets/models/travel_class.py
@@ -3,5 +3,5 @@ from django.utils.translation import gettext_lazy as _
 
 class ClassTypes(models.TextChoices):
         ECONOMIC = 'ECON', _('Economic')
-        EXECUTIVE = 'EXEC', _('Executiva')
+        EXECUTIVE = 'EXEC', _('Executive')
         FIRST_CLASS = 'STCL', _('First class')
